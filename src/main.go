749aa49704cb88bfef98fe7478ff11f911eb03a5
Malcak <malcakx@gmail.com>
2021-03-27 13:15:36 -0500

    explorando las funciones de fmt

--- src/main.go
+++ src/main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hola mundo")
+
+	// declaracion de variables
+	helloMessage := "Hello"
+	worldMessage := "World"
+
+	// print ln
+	fmt.Println(helloMessage, worldMessage)
+
+	// print f
+	nombre := "platzi"
+	cursos := 500
+	
+	fmt.Printf("%s tiene mas de %d cursos\n", nombre, cursos)
+	fmt.Printf("%v tiene mas de %v cursos\n", nombre, cursos)
+
+	// sprint f
+
+	message := fmt.Sprintf("%s tiene mas de %d cursos", nombre, cursos)
+	fmt.Println(message)
+
+	// tipo de datos
+
+	fmt.Printf("helloMessage: %T\n", helloMessage)
+	fmt.Printf("cursos: %T\n", cursos)
+
 }
