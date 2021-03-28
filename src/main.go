76b47b023f442668295759e56309a7d02b23e96c
Malcak <malcakx@gmail.com>
2021-03-27 21:40:31 -0500

    reflect typeof reto

--- src/main.go
+++ src/main.go
@@ -1,42 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+)
 
-type polygon2D interface {
-	area() float64
+type figure2D interface {
+	getArea() float64
 }
 
-type cuadrado struct {
+type square struct {
 	base float64
 }
 
-type rectangulo struct {
-	base float64
-	altura float64
+type rectangle struct {
+	high  float64
+	width float64
 }
 
-func (c cuadrado) area() float64 {
-	return c.base * c.base
+type trapezoid struct {
+	baseA float64
+	baseB float64
+	high  float64
 }
 
-func (r rectangulo) area() float64 {
-	return r.base * r.altura
+type circle struct {
+	radio float64
 }
 
-func calculate(p polygon2D) {
-	fmt.Println("Area:", p.area())
+func (s square) getArea() float64 {
+	return s.base * s.base
+}
+
+func (r rectangle) getArea() float64 {
+	return r.high * r.width
+}
+
+func (t trapezoid) getArea() float64 {
+	return ((t.baseA + t.baseB) / 2) * t.high
+}
+
+func (c circle) getArea() float64 {
+	return math.Pi * math.Pow(c.radio, 2)
+}
+
+func calculateArea(f figure2D) {
+	fmt.Printf("Area of %s: %.2f\n", reflect.TypeOf(f).Name(), f.getArea())
 }
 
 func main() {
-	
-	mySquare := cuadrado{base: 2}
-	myRectangle := rectangulo{base: 2, altura: 4}
-
-	calculate(mySquare)
-	calculate(myRectangle)
-
-	// lista de interfaces
-	myInterface := []interface{}{"Hola", 12, 4.90}
-	fmt.Println(myInterface)
-
+	mySquare := square{base: 5}
+	myRectangle := rectangle{width: 4, high: 5}
+	myTrapezoid := trapezoid{baseA: 18, baseB: 10, high: 5}
+	myCircle := circle{radio: 4}
+	calculateArea(mySquare)
+	calculateArea(myRectangle)
+	calculateArea(myTrapezoid)
+	calculateArea(myCircle)
 }
