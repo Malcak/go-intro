3584b51b411c0f6ee5184a907098676f27643f91
Malcak <malcakx@gmail.com>
2021-03-27 23:28:07 -0500

    manejo de gomodules

--- src/main.go
+++ src/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/labstack/echo"
+	"fmt"
 )
 
 func main() {
+	/*
 	// instanciar echo
 	e := echo.New()
 
@@ -15,4 +14,6 @@ func main() {
 		return c.String(http.StatusOK, "Hello World")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
+	*/
+	fmt.Println("Hello")
 }
