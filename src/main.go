c2bc78d726ffc3113e4a781f00a7eb24838f5e74
Malcak <malcakx@gmail.com>
2021-03-27 21:37:25 -0500

    interfaces y listas de interfaces

--- src/main.go
+++ src/main.go
@@ -2,20 +2,41 @@ package main
 
 import "fmt"
 
-type pc struct {
-	ram int
-	disk int
-	brand string
+type polygon2D interface {
+	area() float64
 }
 
-func (myPC pc) String() string {
-	return fmt.Sprintf("{ram:%d, disk:%d, brand:%s}", myPC.ram, myPC.disk, myPC.brand)
+type cuadrado struct {
+	base float64
+}
+
+type rectangulo struct {
+	base float64
+	altura float64
+}
+
+func (c cuadrado) area() float64 {
+	return c.base * c.base
+}
+
+func (r rectangulo) area() float64 {
+	return r.base * r.altura
+}
+
+func calculate(p polygon2D) {
+	fmt.Println("Area:", p.area())
 }
 
 func main() {
 	
-	myPC := pc{ram: 16, disk: 512, brand: "apple"}
+	mySquare := cuadrado{base: 2}
+	myRectangle := rectangulo{base: 2, altura: 4}
 
-	fmt.Println(myPC)
+	calculate(mySquare)
+	calculate(myRectangle)
+
+	// lista de interfaces
+	myInterface := []interface{}{"Hola", 12, 4.90}
+	fmt.Println(myInterface)
 
 }
