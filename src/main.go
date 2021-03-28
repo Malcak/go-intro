d6364854b5dddb0c7a9ea9cb354e04d97ab42556
Malcak <malcakx@gmail.com>
2021-03-27 22:38:08 -0500

    range, close y select en channels

--- src/main.go
+++ src/main.go
@@ -4,22 +4,39 @@ import (
 	"fmt"
 )
 
-func say (text string, c chan<- string) {
+func message(text string, c chan string) {
 	c <- text
 }
 
-func printTextOurCh(c <-chan string) {
-	fmt.Println(<-c)
-}
-
 func main() {
-	c := make(chan string, 1)
 
-	fmt.Println("Hello")
+	c := make(chan string, 2)
+	c <- "Mensaje 1"
+	c <- "Mensaje 2"
 
-	go say("Bye", c)
+	fmt.Println(len(c), cap(c))
 
-	printTextOurCh(c)
+	// range y close
+	close(c)
+	// c <- "Mensaje 3"
 
+	for message := range c {
+		fmt.Println(message)
+	}
 
+	// select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("mensaje 1", email1)
+	go message("mensaje 2", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <- email1:
+			fmt.Println("Email recibido de Email1", m1)
+		case m2 := <- email2:
+			fmt.Println("Email recibido de Email2", m2)
+		}
+	}
 }
