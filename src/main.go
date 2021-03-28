3c1dd37fcba493f71d962547d248084f1c21c405
Malcak <malcakx@gmail.com>
2021-03-27 23:14:35 -0500

    go modules, hello world con framework echo

--- src/main.go
+++ src/main.go
@@ -1,42 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
-func message(text string, c chan string) {
-	c <- text
-}
-
 func main() {
+	// instanciar echo
+	e := echo.New()
 
-	c := make(chan string, 2)
-	c <- "Mensaje 1"
-	c <- "Mensaje 2"
-
-	fmt.Println(len(c), cap(c))
-
-	// range y close
-	close(c)
-	// c <- "Mensaje 3"
-
-	for message := range c {
-		fmt.Println(message)
-	}
-
-	// select
-	email1 := make(chan string)
-	email2 := make(chan string)
-
-	go message("mensaje 1", email1)
-	go message("mensaje 2", email2)
-
-	for i := 0; i < 2; i++ {
-		select {
-		case m1 := <- email1:
-			fmt.Println("Email recibido de Email1", m1)
-		case m2 := <- email2:
-			fmt.Println("Email recibido de Email2", m2)
-		}
-	}
+	// ruta
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
