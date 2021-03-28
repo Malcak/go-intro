c186abbf2b5d6afef00f31685dff9ed557b179b9
Malcak <malcakx@gmail.com>
2021-03-27 19:03:11 -0500

    array y slice

--- src/main.go
+++ src/main.go
@@ -4,25 +4,28 @@ import "fmt"
 
 func main() {
 
-	// defer
+	// array
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
+	// slice
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-	// continue y break
+	// metodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
 
-	for i := 0; i <10; i++ {
+	// append
+	slice = append(slice, 7)
+	fmt.Println(slice, len(slice), cap(slice))
 
-		if i == 2 {
-			fmt.Printf("Es 2")
-			continue
-		}
-
-		if i == 8 {
-			fmt.Println("Es 8")
-			break
-		}
-
-	}
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice, len(slice), cap(slice))
 
 }
